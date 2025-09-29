053548178daf04cc8f9240701bb82524112c02bc
Arturo <arthursolokhin27@gmail.com>
2025-09-29 03:56:35 +0300

    Initial commit

---
+++ src/main/java/com/github/ar4ik4ik/cloudstorage/CloudStorageApplication.java
@@ -0,0 +1,13 @@
+package com.github.ar4ik4ik.cloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStorageApplication.class, args);
+	}
+
+}
