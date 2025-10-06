65c9a677d17f8a4d9c2293d4837b683be5a6ff5f
Arturo <arthursolokhin27@gmail.com>
2025-10-06 04:04:01 +0300

    feat (test): added unit tests for DatabaseUserDetailsService.java

--- src/main/java/com/github/ar4ik4ik/cloudstorage/entity/Authority.java
+++ src/main/java/com/github/ar4ik4ik/cloudstorage/entity/Authority.java
@@ -2,6 +2,7 @@ package com.github.ar4ik4ik.cloudstorage.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class Authority {
 
     @Id
---
+++ src/test/java/com/github/ar4ik4ik/cloudstorage/service/DatabaseUserDetailsServiceTest.java
@@ -0,0 +1,135 @@
+package com.github.ar4ik4ik.cloudstorage.service;
+
+import com.github.ar4ik4ik.cloudstorage.controller.UserCreateDto;
+import com.github.ar4ik4ik.cloudstorage.entity.Authority;
+import com.github.ar4ik4ik.cloudstorage.entity.AuthorityType;
+import com.github.ar4ik4ik.cloudstorage.entity.User;
+import com.github.ar4ik4ik.cloudstorage.repository.AuthorityRepository;
+import com.github.ar4ik4ik.cloudstorage.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class DatabaseUserDetailsServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private AuthorityRepository authorityRepository;
+
+    @InjectMocks
+    private DatabaseUserDetailsService databaseUserDetailsService;
+
+    @Test
+    @DisplayName("Returns userDetails object when username is exists")
+    void loadUserByUserName_ReturnsUserDetails() {
+
+        // given
+        String username = "testUser";
+        User user = User.builder()
+                .authorities(List.of(Authority.builder()
+                                .name(AuthorityType.ROLE_USER)
+                                .id(1)
+                        .build()))
+                .password("testPassword")
+                .enabled(true)
+                .createdAt(OffsetDateTime.now())
+                .username(username)
+                .build();
+
+        // when
+        when(userRepository.findUserByUsername(username)).thenReturn(Optional.of(user));
+        UserDetails userDetails = databaseUserDetailsService.loadUserByUsername(username);
+
+        // then
+        var expectedAuthorities = List.of(new SimpleGrantedAuthority(AuthorityType.ROLE_USER.getAuthority()));
+
+        assertArrayEquals(expectedAuthorities.toArray(), userDetails.getAuthorities().toArray());
+        assertEquals(user.getUsername(), userDetails.getUsername());
+        assertEquals(user.getPassword(), userDetails.getPassword());
+        assertEquals(user.getEnabled(), userDetails.isEnabled());
+        verify(userRepository).findUserByUsername(username);
+    }
+
+    @Test
+    @DisplayName("Throws exception when user is not exists")
+    void loadUserByUserName_ThrowsUsernameNotFoundException() {
+        // given
+        String notExistingUsername = "notExistingUsername";
+
+        // when
+        when(userRepository.findUserByUsername(notExistingUsername)).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(UsernameNotFoundException.class,
+                () -> databaseUserDetailsService.loadUserByUsername(notExistingUsername));
+
+        verify(userRepository).findUserByUsername(notExistingUsername);
+    }
+
+    @Test
+    @DisplayName("Process user register")
+    void processUserRegister_ReturnsVoid() {
+        // given
+        String username = "testUser";
+        String rawPassword = "testPassword";
+        String encodedPassword = "encodedTestPassword";
+
+        UserCreateDto userCreateDto = new UserCreateDto(username, rawPassword);
+
+        Authority authority = Authority.builder()
+                .id(1)
+                .name(AuthorityType.ROLE_USER)
+                .build();
+
+        when(passwordEncoder.encode(eq(rawPassword))).thenReturn(encodedPassword);
+        when(authorityRepository.getAuthorityByName(eq(AuthorityType.ROLE_USER))).thenReturn(authority);
+        when(userRepository.save(any(User.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        // when
+        databaseUserDetailsService.processUserRegister(userCreateDto);
+
+        // then
+        verify(passwordEncoder, times(1)).encode(eq(rawPassword));
+        verify(authorityRepository, times(1)).getAuthorityByName(eq(AuthorityType.ROLE_USER));
+
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository, times(1)).save(argumentCaptor.capture());
+
+        User user = argumentCaptor.getValue();
+
+        assertNotNull(user);
+        assertEquals(username, user.getUsername());
+        assertEquals(encodedPassword, user.getPassword());
+        assertTrue(user.getEnabled());
+
+        assertNotNull(user.getAuthorities());
+        assertEquals(1, user.getAuthorities().size());
+        assertEquals(authority, user.getAuthorities().getFirst());
+
+
+    }
+
+}
