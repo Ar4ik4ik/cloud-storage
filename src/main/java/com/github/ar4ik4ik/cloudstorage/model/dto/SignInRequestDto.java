5db5ea67a7517b0f10116e962952b4af03c07e3c
Arturo <arthursolokhin27@gmail.com>
2025-12-10 01:38:06 +0300

    fix (sign dtos): add size validation

--- src/main/java/com/github/ar4ik4ik/cloudstorage/model/dto/SignInRequestDto.java
+++ src/main/java/com/github/ar4ik4ik/cloudstorage/model/dto/SignInRequestDto.java
@@ -4,6 +4,6 @@ package com.github.ar4ik4ik.cloudstorage.model.dto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
-public record SignInRequestDto(@NotEmpty @Size(min = 3) String username,
-                               @NotEmpty @Size(min = 8) String password) {
+public record SignInRequestDto(@NotEmpty @Size(min = 3, max = 50) String username,
+                               @NotEmpty @Size(min = 8, max = 100) String password) {
 }
